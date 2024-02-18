2d077a02ded83bbabbb9fe30bb68e873f4f2dfd9
Alekhyagurram11 <158799377+Alekhyagurram11@users.noreply.github.com>
2024-02-18 15:24:58 +0530

    Create Header.cpp

---
+++ week3/Header.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "boxArea.h"
+#include "boxVolume.h"
+
+int main()
+{
+   int length, width, height;
+   cout<<"Enter the length, width and height of the box : "<<endl;
+   cin>>length;
+   cin>>width;
+   cin>>height;
+
+   boxArea(length,width,height);
+   boxVolume(length, width, height);
+}
