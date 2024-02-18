77e4a95a6fce069460bec904a9de01755414007c
Alekhyagurram11 <158799377+Alekhyagurram11@users.noreply.github.com>
2024-02-18 17:00:31 +0530

    Create paramconst.cpp

---
+++ week3/paramconst.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+class student{
+    private:
+        string fullname;
+        int rollnum;
+        double sempercentage;
+        string collegename;
+        int collegecode;
+    public:
+        student()
+        {
+            collegename = "MVGR";
+            collegecode = 33;
+        }
+        student(string name,double percentage)
+        {
+            collegename = "MVGR";
+            collegecode = 33;
+            fullname = name;
+            sempercentage = percentage;
+        }
+        ~student(){}
+        void getstudentdetails()
+        {
+            cout << "The student details are as follows : " << endl;
+            cout << "Name of the student : " << fullname << endl ;
+            cout << "Rollno : " << rollnum << endl ;
+            cout << "College : " << collegename << endl ;
+            cout << "College code : " << collegecode << endl ;
+            cout << "Sem percentage : " << sempercentage << endl ;
+        }
+        void studentrollno(int r)
+        {
+            rollnum = r;
+        }
+};
+
+int main()
+{
+    student s;
+    string name;
+    double sempercent;
+    int rollno;
+    cout << "Enter the name of the student : ";
+    getline(cin,name);
+    cout << "Enter your Roll no : ";
+    cin >> rollno;
+    cout << "Enter your percentage : ";
+    cin >> sempercent;
+    a = {name,sem};
+    a.setstudentrollno(roll);
+    a.getstudentdetails();                  
+}
